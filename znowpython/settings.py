5ce642782863c92591dc61af2e2a226d46bf498c
Daniel <znowm4n@gmail.com>
2023-03-03 20:56:15 +0100

    Added migrawtions, updated views

--- znowpython/settings.py
+++ znowpython/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'znow.apps.ZnowConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- znowpython/urls.py
+++ znowpython/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from znow import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
